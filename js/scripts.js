fe06c1bbfd1e3927d9fb26c04971ce90f614b5a7
Russell Hofvendahl <rhofvendahl@gmail.com>
2018-01-19 10:58:51 -0800

    add image to canvas

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+  var canvas = document.getElementById("canvas");
+  var ctx=canvas.getContext("2d");
+
+  var perlin1 = document.getElementById("perlin1");
+
+  ctx.drawImage(perlin1, 0, 0);
 
 });

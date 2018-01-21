f73612486ab3aabd7dfefcb39f46ad936ffb12c8
Russell Hofvendahl <rhofvendahl@gmail.com>
2018-01-21 12:46:06 -0800

    add updateMap logic

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,7 @@ function makeMap(xDim, zDim) {
   var map = new Array(xDim);
   peak = 0;
   for (var x=0; x<xDim; x++) {
-    map[x] = new Array(zDim);
-    for (var z=0; z<zDim; z++) {
-      map[x][z] = 0;
-      map[x][z] +=Math.random()/4 +2.5 - Math.pow(Math.pow(x-6,2)+Math.pow(z-6, 2), 1/5);
-      map[x][z] += 2.5 - Math.pow(Math.pow(x-14,2)+Math.pow(z-16, 2), 1/5);
-      //map[x][y] += (2.5 - Math.pow(Math.pow(x-10,2)+Math.pow(y-10, 2), 1/5))/2;
-      if (map[x][z] > peak) peak = map[x][z];
-      console.log(peak);
-    };
+    map[x] = new Array(zDim).fill(0);
   };
   return map;
 };
@@ -37,15 +29,15 @@ function makeBox(className, xCoord, yCoord, zCoord, xDim, yDim, zDim) {
   //comment sides to reduce latency
   //orthogonal to y axis (-y is up)
   makeFace(className, "light", xDim, zDim,  (xCoord+xDim/2), -(yCoord+yDim), (zCoord+zDim/2), 90, 0, 0);
-  makeFace(className, "dark", xDim, zDim, (xCoord+xDim/2), -(yCoord), (zCoord+zDim/2), 90, 0, 0);
+  //makeFace(className, "dark", xDim, zDim, (xCoord+xDim/2), -(yCoord), (zCoord+zDim/2), 90, 0, 0);
 
   //orthogonal to z axis
-  makeFace(className, "dark", xDim, yDim, (xCoord+xDim/2), -(yCoord+yDim/2), (zCoord+zDim), 0, 0, 0);
-  makeFace(className, "medium", xDim, yDim, (xCoord+xDim/2), -(yCoord+yDim/2), (zCoord), 0, 0, 0);
+  makeFace(className, "medium", xDim, yDim, (xCoord+xDim/2), -(yCoord+yDim/2), (zCoord+zDim), 0, 0, 0);
+  //makeFace(className, "medium", xDim, yDim, (xCoord+xDim/2), -(yCoord+yDim/2), (zCoord), 0, 0, 0);
 
   //orthogonal to x axis
-  makeFace(className, "medium", zDim, yDim, (xCoord + xDim), -(yCoord+yDim/2), (zCoord+zDim/2), 0, (90), 0);
-  makeFace(className, "dark", zDim, yDim, xCoord, -(yCoord+yDim/2), (zCoord+zDim/2), 0, (90), 0);
+  //makeFace(className, "medium", zDim, yDim, (xCoord + xDim), -(yCoord+yDim/2), (zCoord+zDim/2), 0, (90), 0);
+  //makeFace(className, "dark", zDim, yDim, xCoord, -(yCoord+yDim/2), (zCoord+zDim/2), 0, (90), 0);
 };
 
 function makeFace(className, shade, width, height, tx, ty, tz, rx, ry, rz) {
@@ -62,52 +54,58 @@ function makeFace(className, shade, width, height, tx, ty, tz, rx, ry, rz) {
 function updateScape(map, cellDim, peak) {
   for (var x=0; x<map.length; x++) {
     for (var z=0; z<map[0].length; z++) {
-      var box = $("." + x + "-" + z)
-    //  $(".base").animate({"top": "-=" + Math.random()*cellDim*2 + "px"}, 5000);
+      var box = $("." + x + "-" + z);
+    $("." + x + "-" + z).animate({"top": -map[x][z]*cellDim + "px"}, 5000);
 
       //box.css("transform", "translateZ(" + map[x][y]*cellDim + "px)");
-      if (map[x][z] < .1*peak) {
+      //console.log("updateScape x" + x + "z" + z + " " + map[x][z]);
+
+      if (map[x][z] <= .2*peak) {
         //blue
         box.children(".light").css("background", "#9CF1FD");
         box.children(".medium").css("background", "#75B4BC");
         box.children(".dark").css("background", "#50787E");
-      } else if (map[x][z] < .2*peak) {
+      } else if (map[x][z] <= .25*peak) {
         //tan
         box.children(".light").css("background", "#FFF089");
         box.children(".medium").css("background", "#C1B367");
         box.children(".dark").css("background", "#817847");
-      } else if (map[x][z] < .4*peak) {
+      } else if (map[x][z] <= .5*peak) {
         //green
         box.children(".light").css("background", "#CFFD78");
         box.children(".medium").css("background", "#9BBC5B");
         box.children(".dark").css("background", "#687E3E");
-      } else if (map[x][z] < .7*peak) {
+      } else { //if (map[x][z] <= .7*peak) {
         //grey
         box.children(".light").css("background", "#BEBEBE");
         box.children(".medium").css("background", "#8E8E8E");
         box.children(".dark").css("background", "#606060");
-      } else {
+      }; /*else {
         //white
         box.children(".light").css("background", "#FFFFFF");
         box.children(".medium").css("background", "#BEBEBE");
         box.children(".dark").css("background", "#7F7F7F");
-      };
+      };*/
     };
   };
 };
 
-function updateMap(map, cellDim, clickEvents) {
+function updateMap(map, cellDim, click) {
+  var peak = 0;
   for (var x=0; x<map.length; x++) {
     for (var z=0; z<map[0].length; z++) {
-      clickEvents.forEach(function(click) {
-        var clickAge = (Date.now() - click[3])/1000
-        if (clickAge < 10) {
-          var clickDistance=Math.sqrt(Math.pow(x-click[0],2)+Math.pow(z-click[1]));
-          map[x][z] = click[3]*cellDim*Math.random()/(clickDistance*clickAge);
-        };
-      });
+      var distance1 = Math.pow((Math.pow(x-10, 2)+Math.pow(z-10, 2)), 1/2);
+      var distance2 = Math.pow((Math.pow(x-15, 2)+Math.pow(z-15, 2)), 1/2);
+
+      map[x][z] = Math.random()/8
+      map[x][z] += 1/(1+distance1);///42.5 - Math.pow(Math.pow(x-click[0])+Math.pow(z-click[1], 2), 1/);
+      map[x][z] += 1/(1+distance2);
+      if (map[x][z] > peak) peak = map[x][z];
     };
   };
+  console.log(peak);
+
+  return peak;
 };
 //THREE SIDES
 //1000x1000 at px2 is too much
@@ -144,31 +142,16 @@ $(document).ready(function() {
   var map = makeMap(xCells, zCells);
   makeScape(map, cellDim);
   //peak global for now
-  updateScape(map, cellDim, peak);
 
-  var clickCount = 0;
-  var clickEvents = new Array(5);
   $("#scape").children().click(function() {
-    var clickEvent = new Array(4);
-    //x coord
-    clickEvent[0] = parseInt($(this).attr("class").split(/[- ]/)[0]);
-    //z coord
-    clickEvent[1] = parseInt($(this).attr("class").split(/[- ]/)[1]);
-    //magnitude
-    clickEvent[2] = Number.parseFloat(Math.random().toFixed(3));
-    //timeStamp
-    clickEvent[3] = Date.now();
-    clickEvents[clickCount%clickEvents.length] = clickEvent;
-    clickCount++;
-    console.log(clickEvent);
+    var click = new Array(4);
+    click[0] = parseInt($(this).attr("class").split(/[- ]/)[0]);
+    click[1] = parseInt($(this).attr("class").split(/[- ]/)[1]);
+    click[2] = Number.parseFloat(Math.random().toFixed(3));
+    click[3] = Date.now();
+    console.log(click);
+    var peak = updateMap(map, cellDim, click);
+    updateScape(map, cellDim, peak);
+
   });
-
-  //$("#scape").animate({"margin-top": "-=30px"}, 5000);
-  /*function rrender(map, cellDim, clickEvents) {
-    updateMap(map, cellDim, clickEvents);
-    updateScape(map, cellDim);
-  };
-
-  setInterval(render, 5000);
-*/
 });

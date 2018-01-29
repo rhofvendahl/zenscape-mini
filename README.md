fd1325123a5f1527ad93788e3ed67f58087827d1
emile ward <emileward@Camilles.domain>
2018-01-28 23:40:10 -0800

    create Zenscape constructor, with makeMap, updateMap, makeScape and updateScape as methods. pass as parameter to setInterval to eliminate globals.

--- README.md
+++ README.md
@@ -8,6 +8,26 @@
 
 Zenscape is a web application built to explore the potential of css transforms to create rich three dimensional models. It was inspired by the work of Keith Clark (keithclark.co.uk) and the desire to model geological processes to create an aesthetically pleasing user experience.
 
+
+## Specs
+
+####Back-end
+
+1. Program defines a function "makeFace" for creating a new div element with dimensions, xyz rotation and xyz translation as arguments.
+2. Program defines a function "makeBox" for creating 6 faces to form an enclosed box with box dimensions and translation as arguments. By default all but the front two faces are commented out to reduce rendering load.
+3. Program defines a constructor for a Zenscape object with properties "xCells" and "zCells" (number of independent cells), "cellDim" (length of cubical cell in px), "map" (to contain an array of cell y values) and "clickLog" (to contain an array of the location and timestamps for the last five clicks) and "memory" (the maximum number of clicks considered calculated).
+4. Program defines a Zenscape.prototype function "makeMap" for creating an appropriately dimensioned 2d array "map" with 0 for each value.
+6. Program defines a Zenscape.prototype function "makeScape" for creating an array of div cells using the "makeBox" function, with the y axis extending down, the x axis extending right and the z axis extending toward the screen.
+7. Program defines a Zenscape.prototype function "updateMap" which iterates through the 2d "map" array and assigns each a value representing y position based on the cell's location relative to the last x clicks (where x is set by "memory") and the time since each. For each cell and for each click the distance between cell and click is calculated and, if it is less than pi, the height of that cell is calculated based on the cosine of that number (keeping it under pi means only cells within the first "hill" of the cosine curve centered at the click are calculated). The effective spread of the cosine curve is doubled by dividing distance by 2, and the hill effectively moves outward from the click epicenter by subtracting the age of the click in seconds and only calculating cells for which distance/2-seconds is between -pi and pi; a cell at the epicenter of the click at time 0 would have a height of cos(0) (one), after a second it would a slightl lower height but all adjacent cells would have a slightl higher value as distance/2-seconds approaches 0, and after that their hight would also diminish as distance/2-seconds becomes negative and eventually ceases to be calculated after falling below -pi. 
+8. Program defines a Zenscape.prototype function "updateScape" which for each box of divs first animates the top margin based on the corresponding value in "map". UpdateScape then assigns the group of divs a color based on each div's shade tag and the "map" value, with colors assigned with respect to an arbitrary "snowLine".
+  
+
+#### Front-end
+
+9. Program creates a new Zenscape object and passes to it xCells, zCells, cellDim and memory. Program calls Zenscape methods makeMap and makeScape to set up the 2d map array and 3d div formation.
+10. Program attaches an event handler to parent div (id="scape") which, for any cell clicked, references the clicked div's class (named by relative position, eg class="0-0" or "19-19"), saves the extracted coordinates and a timestamp in an array "click", and pushes the "click" array onto the "clickLog" array in Zenscape.
+11. Program calls setInterval (200 ms) to create an animation loop and passes to it a Zenscape object and a function which stops all previous animations (to prevent queue buildup), calls updateMap and subsequently calls updateScape to apply changes.
+
 ## Setup/Installation Requirements
 
 Zenscape is viewable online at zenscape.us
@@ -19,7 +39,7 @@ To view the source code:
 
 ## Technologies Used
 
-Zenscape was created entirely with html and css styling, the only additional libraries being Bootstrap and JQuery
+Zenscape was created entirely with html and css styling, as well as the JQuery library.
 
 ## License
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,3 @@
-function makeMap(xDim, zDim) {
-  map = new Array(xDim);
-  for (var x=0; x<xDim; x++) {
-    map[x] = new Array(zDim).fill(0);
-  };
-};
-
-function makeScape() {
-  $("#scape").css("margin-left", (-map.length*cellDim/2) + "px");
-  $("#scape").css("margin-top", (-map[0].length*cellDim/2) + "px");
-  for (x=0; x<map.length; x++) {
-    for (z=0; z<map[0].length; z++) {
-      makeBox((x + "-" + z), x*cellDim, map[x][z]*cellDim, z*cellDim, cellDim, cellDim, cellDim);
-    };
-  };
-  makeBox("base", 0, (-cellDim*1.5), 0, (map.length*cellDim), cellDim, (map[0].length*cellDim));
-};
-
 function makeBox(className, xCoord, yCoord, zCoord, xDim, yDim, zDim) {
   //console.log("makeBox");
   $("#scape").append("<div class='" + className + " object'></div>");
@@ -35,7 +17,6 @@ function makeBox(className, xCoord, yCoord, zCoord, xDim, yDim, zDim) {
 };
 
 function makeFace(className, shade, width, height, tx, ty, tz, rx, ry, rz) {
-  //console.log(className);
   $("." + className).append("<div class='" + shade + " object'></div>");
   var side = $("." + className).children().last();
   side.css("width", width);
@@ -45,28 +26,69 @@ function makeFace(className, shade, width, height, tx, ty, tz, rx, ry, rz) {
   side.css("transform", "translate3d("+tx+"px, "+ty+"px,"+tz+"px) rotateX("+rx+"deg) rotateY("+ry+"deg) rotateZ("+rz+"deg)");
 };
 
-function updateScape() {
+function Zenscape(xCells, zCells, cellDim, memory) {
+	this.xCells = xCells;
+	this.zCells = zCells;
+	this.cellDim = cellDim;
+	this.map = undefined;
+	this.clickLog = [];
+	this.memory = memory;
+};
+
+Zenscape.prototype.makeMap = function() {
+  this.map = new Array(this.xCells);
+  for (var x=0; x<this.xCells; x++) {
+    this.map[x] = new Array(this.zCells).fill(0);
+  };
+};
+
+Zenscape.prototype.makeScape = function() {
+  $("#scape").css("margin-left", (-this.map.length*this.cellDim/2) + "px");
+  $("#scape").css("margin-top", (-this.map[0].length*this.cellDim/2) + "px");
+  for (x=0; x<this.map.length; x++) {
+    for (z=0; z<this.map[0].length; z++) {
+      makeBox((x + "-" + z), x*this.cellDim, this.map[x][z]*this.cellDim, z*this.cellDim, this.cellDim, this.cellDim, this.cellDim);
+    };
+  };
+  makeBox("base", 0, (-this.cellDim*1.5), 0, (this.map.length*this.cellDim), this.cellDim, (this.map[0].length*this.cellDim));
+};
+
+Zenscape.prototype.updateMap = function() {
+  for (var x=0; x<this.map.length; x++) {
+    for (var z=0; z<this.map[0].length; z++) {
+      this.map[x][z] = 0;
+      for (var i = 0; (i < this.memory) && (i < this.clickLog.length); i++) {
+        var click = this.clickLog[this.clickLog.length-1-i];
+        var seconds = (Date.now() - click[2])/1000;
+        var distance = Math.pow((Math.pow(x-click[0], 2)+Math.pow(z-click[1], 2)), 1/2);
+        if (Math.abs(distance/2 - seconds) < Math.PI) this.map[x][z] += (Math.cos(distance/2 - seconds) + 1)/2;
+      };
+    };
+  };
+};
+
+Zenscape.prototype.updateScape = function() {
   var snowLine = 3;
-  for (var x=0; x<map.length; x++) {
-    for (var z=0; z<map[0].length; z++) {
+  for (var x=0; x<this.map.length; x++) {
+    for (var z=0; z<this.map[0].length; z++) {
       var box = $("." + x + "-" + z);
-    $("." + x + "-" + z).animate({top: -map[x][z]*cellDim + "px"}, 250);
-      if (map[x][z] <= .2*snowLine) {
+    $("." + x + "-" + z).animate({top: -this.map[x][z]*this.cellDim + "px"}, 250);
+      if (this.map[x][z] <= .2*snowLine) {
         //blue
         box.children(".light").css("background", "#4081f2");
         box.children(".medium").css("background", "#346dc7");
         box.children(".dark").css("background", "#275799");
-      } else if (map[x][z] <= .3*snowLine) {
+      } else if (this.map[x][z] <= .3*snowLine) {
         //tan
         box.children(".light").css("background", "#FFF089");
         box.children(".medium").css("background", "#C1B367");
         box.children(".dark").css("background", "#817847");
-      } else if (map[x][z] <= .6*snowLine) {
+      } else if (this.map[x][z] <= .6*snowLine) {
         //green
         box.children(".light").css("background", "#2aa330");
         box.children(".medium").css("background", "#1f8c28");
         box.children(".dark").css("background", "#106e1f");
-      } else if (map[x][z] <= snowLine) {
+      } else if (this.map[x][z] <= snowLine) {
         //grey
         box.children(".light").css("background", "#BEBEBE");
         box.children(".medium").css("background", "#8E8E8E");
@@ -81,62 +103,22 @@ function updateScape() {
   };
 };
 
-function updateMap() {
-  for (var x=0; x<map.length; x++) {
-    for (var z=0; z<map[0].length; z++) {
-      map[x][z] = 0;
-      for (var i = 0; (i < 5) && (i < clickLog.length); i++) {
-        var click = clickLog[clickLog.length-1-i];
-        var seconds = (Date.now() - click[2])/1000;
-        var distance = Math.pow((Math.pow(x-click[0], 2)+Math.pow(z-click[1], 2)), 1/2);
-        if (Math.abs(distance/2 - seconds) < Math.PI) map[x][z] += (Math.cos(distance/2 - seconds) + 1)/2;
-      };
-    };
-  };
-};
-//THREE SIDES
-//1000x1000 at px2 is too much
-//1000x1000 at 2px also too much
-//10x10 at 2px is fine
-//10x10 10px fine
-//40x40 10px laggy
-//**40x40 20px smooth (laggy when it passes camera)**
-//40x40 20px no perspective quite smooth
-//100x100 20px no perspective doesn't load
-//100x100 2px no perspective doesn't load
-//60x60 2px no perspective some latency
-//60x60 8px no perspective significant latency
-//*50x50 8px no perspective slight latency*
-//40x40 16px no perspective smooth
-//40x40 20px no perspective quite smooth
-//**40x40 40px no perspective quite smooth**
-//*50x50 8px no perspective smooth*
-//ONE SIDES
-//**100x100 4px no perspective loads slowly, but smooth**
-//*100x100 10px no perspective 8s load, pretty smooth*
-
-var map;
-var clickLog = [];
-var cellDim;
-
 $(document).ready(function() {
-  var xCells = 20;
-  var zCells = 20;
-  cellDim = 20;
-  makeMap(xCells, zCells);
-  makeScape();
+  var zs = new Zenscape(20, 20, 20, 5);
+  zs.makeMap();
+  zs.makeScape();
 
   $("#scape").children().click(function() {
     var click = new Array(3);
     click[0] = parseInt($(this).attr("class").split(/[- ]/)[0]);
     click[1] = parseInt($(this).attr("class").split(/[- ]/)[1]);
     click[2] = Date.now();
-    clickLog.push(click);
+    zs.clickLog.push(click);
   });
-
+  
   setInterval(function() {
     $("#scape").children().stop();
-    updateScape();
-    updateMap();
-  }, 200);
+    zs.updateMap();
+    zs.updateScape();
+  }, 200, zs);
 });
